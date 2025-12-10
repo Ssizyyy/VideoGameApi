f39fc6cdebecb7b1d992c47c331457629839c3ff
Hossein <ssizyyt1@gmail.com>
2025-12-10 22:25:50 +0330

    Added DbContext / Code First Migration

--- VideoGameApi/obj/Debug/net10.0/VideoGameApi.AssemblyInfo.cs
+++ VideoGameApi/obj/Debug/net10.0/VideoGameApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoGameApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07e755484d8f3a77a9cc7d4e46d3ac938f8dc52a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c08d2532cd28aa50992effb323471b90b0866d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoGameApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoGameApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
